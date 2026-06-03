9acb5f7e9532b57b4abce57550eeb906fbc0278a
Akihiro1028Bad <ttmakhr1028@gmail.com>
2026-06-03 09:20:54 +0900

    feat(hyrox): コーチ/ギャラリーをページに挿入

--- src/app/[locale]/hyrox/HyroxContent.tsx
+++ src/app/[locale]/hyrox/HyroxContent.tsx
@@ -4,7 +4,9 @@ import HomeNavigation from "@/components/home/HomeNavigation";
 import HomeFooter from "@/components/home/HomeFooter";
 import HyroxHero from "@/components/hyrox/HyroxHero";
 import HyroxIntro from "@/components/hyrox/HyroxIntro";
+import HyroxCoach from "@/components/hyrox/HyroxCoach";
 import HyroxStations from "@/components/hyrox/HyroxStations";
+import HyroxGallery from "@/components/hyrox/HyroxGallery";
 import HyroxProgram from "@/components/hyrox/HyroxProgram";
 
 export default function HyroxContent() {
@@ -14,7 +16,9 @@ export default function HyroxContent() {
       <main>
         <HyroxHero />
         <HyroxIntro />
+        <HyroxCoach />
         <HyroxStations />
+        <HyroxGallery />
         <HyroxProgram />
       </main>
       <HomeFooter />
